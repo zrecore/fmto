a201bbe5fed83c94f56e2f384131416867e381f2
Alejandro Albino <webmaster@alexventure.com>
2024-08-14 13:12:17 -0700

    Refactoring linear algebra crossProduct and dotProduct code to use default compute processing pipeline setup

--- src/math/linearAlgebra/dotProduct.mjs
+++ src/math/linearAlgebra/dotProduct.mjs
@@ -5,7 +5,7 @@
  * @version 1.0.0
  */
 
-import { Vector2D, Vector3D, Vector4D } from "../../index.mjs"
+import { DefaultProcessing, Vector2D, Vector3D, Vector4D } from "../../index.mjs"
 
 /**
  * Return the dot product of two vectors.
@@ -46,113 +46,14 @@ async function dotProduct(device, e1, e2)
         output = dot(e1, e2);
     }
     `
-    const shaderModule = device.createShaderModule({
-        code: shader
-    })
-    // Create buffers to handle our data
-    // ... Specified as storage buffer (high speed).
-    // ... Will be the source of a copy operation
-    const output = device.createBuffer({
-        size: BUFFER_SIZE,
-        usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_SRC
-    })
-    // ... Specified as a buffer that can be mapped for reading by JavaScript
-    // ... Will be the destination of a copy operation
-    const stagingBuffer = device.createBuffer({
-        size: BUFFER_SIZE,
-        usage: GPUBufferUsage.MAP_READ | GPUBufferUsage.COPY_DST
-    })
-
-    // Create a bind group layout
-    const bindGroupLayout = device.createBindGroupLayout({
-        entries: [
-            {
-                binding: 0,
-                visibility: GPUShaderStage.COMPUTE,
-                buffer: {
-                    type: "storage"
-                }
-            }
-        ]
-    })
-
-    const bindGroup = device.createBindGroup({
-        layout: bindGroupLayout,
-        entries: [
-            {
-                binding: 0,
-                resource: {
-                    buffer: output
-                }
-            }
-        ]
-    })
-
-    // Create a compute pipeline
-    const computePipeline = device.createComputePipeline({
-        layout: device.createPipelineLayout({
-            bindGroupLayouts: [
-                bindGroupLayout
-            ]
-        }),
-        compute: {
-            module: shaderModule,
-            entryPoint: "main"
-        }
-    })
-
-    // Run the compute pass
-
-    // ... Create the GPUCommandEncoder to encode commands
-    // ... The commands will be issued to the GPU
-    const commandEncoder = device.createCommandEncoder()
-    // ... Initiate compute pass
-    device.pushErrorScope('validation')
-    const passEncoder = commandEncoder.beginComputePass()
-
-    // (continue running a compute pass)
-    passEncoder.setPipeline(computePipeline)
-    passEncoder.setBindGroup(0, bindGroup)
-    passEncoder.dispatchWorkgroups(Math.ceil(BUFFER_SIZE / 64))
-
-    passEncoder.end()
-
-    device.popErrorScope().then((error) => {
-        if (error)
-        {
-            throw 'src/math/linearAlgebra/dotProduct.mjs shader error:' + error.message
-        }
-    })
-
-    commandEncoder.copyBufferToBuffer(
-        output,
-        0,
-        stagingBuffer,
-        0,
-        BUFFER_SIZE
+    const processing = new DefaultProcessing(
+        device,
+        BUFFER_SIZE,
+        shader
     )
 
-    // End frame by passing array of command buffers to
-    // command queue for execution
-    device
-        .queue
-        .submit([
-            commandEncoder.finish()
-        ])
+    const result = await processing.run()
     
-    // Map staging buffer to read results back into JavaScript
-    await stagingBuffer.mapAsync(
-        GPUMapMode.READ,
-        0,              // Offset
-        BUFFER_SIZE     // Length
-    )
-
-    const copyArrayBuffer = stagingBuffer.getMappedRange(0, BUFFER_SIZE)
-    const data = copyArrayBuffer.slice()
-
-    stagingBuffer.unmap()
-    const result = new Float32Array(data)
-
     return result
 }
 
